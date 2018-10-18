0a21cc17d49c1c8623b8d698637841b034421aca
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-10-18 14:49:44 +0900

    test: use toMatchObject, remove nested describe

--- src/modules/counter/module.spec.ts
+++ src/modules/counter/module.spec.ts
@@ -1,27 +1,26 @@
 import { reducer } from './module';
 
-describe('modules/counter', () => {
-  test('should return the initial state', () => {
-    let state;
-    state = reducer(undefined, { type: undefined, payload: undefined });
-    expect(state).toEqual({ count: 0 });
-  });
+// reducer
+test('should return the initial state', () => {
+  let state;
+  state = reducer(undefined, {} as any);
+  expect(state).toMatchObject({ count: 0 });
+});
 
-  test('should handle INCREMENT', () => {
-    let state;
-    state = reducer(
-      { count: 0 },
-      { type: 'counter/increment', payload: { amount: 3 } },
-    );
-    expect(state).toEqual({ count: 3 });
-  });
+test('should handle INCREMENT', () => {
+  let state;
+  state = reducer(
+    { count: 0 },
+    { type: 'counter/increment', payload: { amount: 3 } },
+  );
+  expect(state).toMatchObject({ count: 3 });
+});
 
-  test('should handle DECREMENT', () => {
-    let state;
-    state = reducer(
-      { count: 5 },
-      { type: 'counter/decrement', payload: { amount: 3 } },
-    );
-    expect(state).toEqual({ count: 2 });
-  });
+test('should handle DECREMENT', () => {
+  let state;
+  state = reducer(
+    { count: 5 },
+    { type: 'counter/decrement', payload: { amount: 3 } },
+  );
+  expect(state).toMatchObject({ count: 2 });
 });
