1172614ef35dcb51e2d2119fbacc0fbe7aa93c87
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-10-23 14:33:39 +0900

    feat: implement async counter epic

---
+++ src/modules/counter/epics.ts
@@ -0,0 +1,15 @@
+import 'rxjs';
+import { map, delay } from 'rxjs/operators';
+import { ofType, Epic } from 'redux-observable';
+import { INCREMENT, ASYNC_INCREMENT } from './actions';
+
+// TODO: more strict type
+export const asyncIncrementEpic: Epic = action$ =>
+  action$.pipe(
+    ofType(ASYNC_INCREMENT),
+    delay(1000),
+    map(action => ({
+      type: INCREMENT,
+      payload: action.payload,
+    })),
+  );
