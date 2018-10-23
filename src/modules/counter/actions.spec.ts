262ecbff85901e321b28a9f3616c8b57189a6ae2
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-10-23 15:57:51 +0900

    test: add counter actions

---
+++ src/modules/counter/actions.spec.ts
@@ -0,0 +1,31 @@
+import * as actions from './actions';
+
+test('should create an action to increment counter', () => {
+  const expectedAction = {
+    type: actions.INCREMENT,
+    payload: {
+      amount: 1,
+    },
+  };
+  expect(actions.incrementAmount(1)).toEqual(expectedAction);
+});
+
+test('should create an action to decrement counter', () => {
+  const expectedAction = {
+    type: actions.DECREMENT,
+    payload: {
+      amount: 4,
+    },
+  };
+  expect(actions.decrementAmount(4)).toEqual(expectedAction);
+});
+
+test('should create an action to async increment counter', () => {
+  const expectedAction = {
+    type: actions.ASYNC_INCREMENT,
+    payload: {
+      amount: 7,
+    },
+  };
+  expect(actions.asyncIncrementAmount(7)).toEqual(expectedAction);
+});
