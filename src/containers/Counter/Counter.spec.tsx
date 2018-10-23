063ddd77e6ad7cf62dc797cb204e62ec0456a377
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-10-23 17:56:07 +0900

    test: add Counter Container

---
+++ src/containers/Counter/Counter.spec.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import reduxMockStore from 'redux-mock-store';
+import { shallow } from 'enzyme';
+import { Counter } from './Counter';
+
+const mockStore = reduxMockStore();
+let store: any;
+let wrapper: any;
+
+beforeEach(() => {
+  store = mockStore({
+    counter: {
+      count: 5,
+    },
+  });
+  store.dispatch = jest.fn();
+  wrapper = shallow(<Counter store={store}>bar</Counter>);
+});
+
+test('should map state and dispatch to props', () => {
+  expect(wrapper.props()).toEqual(
+    expect.objectContaining({
+      count: 5,
+    }),
+  );
+});
+
+test('should map setUserName to dispatch INCREMENT action', () => {
+  wrapper.props().increment();
+
+  expect(store.dispatch).toHaveBeenCalledWith({
+    type: 'counter/increment',
+    payload: {
+      amount: 3,
+    },
+  });
+});
