f45b9fafbf180478cbf15c53686bd417eae0f6e7
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-05 10:11:39 +0900

    reafctor: change to use ReturnType

--- src/modules/counter/actions.ts
+++ src/modules/counter/actions.ts
@@ -1,33 +1,28 @@
-import { Action } from 'redux';
-
 export const INCREMENT = 'counter/increment';
 export const DECREMENT = 'counter/decrement';
 export const ASYNC_INCREMENT = 'counter/async_increment';
 
-export interface Actions
-  extends Action<typeof INCREMENT | typeof DECREMENT | typeof ASYNC_INCREMENT> {
-  payload: {
-    amount: number;
-  };
-}
-
-export const incrementAmount = (amount: number): Actions => ({
-  type: INCREMENT,
+export const incrementAmount = (amount: number) => ({
+  type: INCREMENT as typeof INCREMENT,
   payload: {
     amount,
   },
 });
 
-export const decrementAmount = (amount: number): Actions => ({
-  type: DECREMENT,
+export const decrementAmount = (amount: number) => ({
+  type: DECREMENT as typeof DECREMENT,
   payload: {
     amount,
   },
 });
 
-export const asyncIncrementAmount = (amount: number): Actions => ({
-  type: ASYNC_INCREMENT,
+export const asyncIncrementAmount = (amount: number) => ({
+  type: ASYNC_INCREMENT as typeof ASYNC_INCREMENT,
   payload: {
     amount,
   },
 });
+
+export type Actions = ReturnType<
+  typeof incrementAmount | typeof decrementAmount | typeof asyncIncrementAmount
+>;
--- src/modules/todo/actions.ts
+++ src/modules/todo/actions.ts
@@ -1,32 +1,20 @@
-import { Action } from 'redux';
-
 export const ADD_TODO = 'todo/add_todo';
 export const TOGGLE_TODO = 'todo/toggle_todo';
 
-export interface AddTodo extends Action<typeof ADD_TODO> {
-  payload: {
-    text: string;
-  };
-}
-
-export interface ToggleTodo extends Action<typeof TOGGLE_TODO> {
-  payload: {
-    id: number;
-  };
-}
-
-export type Actions = AddTodo | ToggleTodo;
-
-export const addTodo = (text: string): AddTodo => ({
-  type: ADD_TODO,
+export const addTodo = (text: string) => ({
+  type: ADD_TODO as typeof ADD_TODO,
   payload: {
     text,
   },
 });
 
-export const toggleTodo = (id: number): ToggleTodo => ({
-  type: TOGGLE_TODO,
+export const toggleTodo = (id: number) => ({
+  type: TOGGLE_TODO as typeof TOGGLE_TODO,
   payload: {
     id,
   },
 });
+
+export type Actions =
+  | ReturnType<typeof addTodo>
+  | ReturnType<typeof toggleTodo>;
