ce680d049e458dfa0e8e22427f3311fc9a17ff49
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-13 08:57:24 +0900

    refactor: change babel config to JavaScript file

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+const presets = [
+  "@babel/react",
+  "@babel/typescript",
+  [
+    "@babel/env",
+    {
+      "modules": false
+    }
+  ],
+];
+
+const plugins = [
+  "@babel/proposal-class-properties",
+  "@babel/proposal-object-rest-spread",
+];
+
+process.env["NODE_ENV"] === 'production' && plugins.push("react-remove-properties");
+
+module.exports = { presets, plugins };
