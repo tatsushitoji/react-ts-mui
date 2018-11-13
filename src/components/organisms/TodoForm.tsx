924db7eaf2a100bfdb0c098c9d88ec1375d3eb62
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-13 11:44:36 +0900

    refactor: separated TodoForm as a organisms

---
+++ src/components/organisms/TodoForm.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { pure } from 'recompose';
+import { withFormik, InjectedFormikProps } from 'formik';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import { ITodo } from '../../modules/todo';
+
+export interface Props {
+  todos: ITodo[];
+  add: (_: string) => void;
+  asyncAdd: (_: string) => void;
+  toggle: (_: React.ChangeEvent) => void;
+}
+
+interface FormProps {
+  add: Props['add'];
+  asyncAdd: Props['asyncAdd'];
+}
+
+interface FormValues {
+  text: string;
+}
+
+const InnerForm: React.SFC<
+  InjectedFormikProps<FormProps, FormValues>
+> = props => {
+  const clickAdd = (_: React.MouseEvent) => {
+    props.add(props.values.text);
+    props.submitForm();
+  };
+  const clickAsyncAdd = (_: React.MouseEvent) => {
+    props.asyncAdd(props.values.text);
+    props.submitForm();
+  };
+  return (
+    <form>
+      <TextField
+        label="add todo"
+        name="text"
+        value={props.values.text}
+        placeholder="input text"
+        // InputLabelProps={{
+        //   shrink: true,
+        // }}
+        onChange={props.handleChange}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        onClick={clickAdd}
+        disabled={props.values.text.length <= 0 || props.isSubmitting}
+      >
+        add
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        onClick={clickAsyncAdd}
+        disabled={props.values.text.length <= 0 || props.isSubmitting}
+      >
+        async add
+      </Button>
+      <Button variant="contained" color="secondary" onClick={props.handleReset}>
+        reset
+      </Button>
+    </form>
+  );
+};
+
+const EnhancedForm = withFormik<FormProps, FormValues>({
+  mapPropsToValues: () => ({ text: '' }),
+  handleSubmit: (values, { setSubmitting, resetForm }) => {
+    if (values.text.trim().length <= 0) return;
+    resetForm();
+    setSubmitting(false);
+  },
+})(InnerForm);
+
+export const TodoForm = pure(EnhancedForm);
--- src/components/pages/Todo/Todo.tsx
+++ src/components/pages/Todo/Todo.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import { pure } from 'recompose';
-import { withFormik, InjectedFormikProps } from 'formik';
 import { PaperContainer } from '../../atoms/PaperContainer';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import { TodoList } from '../../organisms/TodoList';
-
+import { TodoForm } from '../../organisms/TodoForm';
 import { ITodo } from '../../../modules/todo';
 
 export interface Props {
@@ -14,74 +10,6 @@ export interface Props {
   asyncAdd: (_: string) => void;
   toggle: (_: React.ChangeEvent) => void;
 }
-
-interface FormProps {
-  add: Props['add'];
-  asyncAdd: Props['asyncAdd'];
-}
-
-interface FormValues {
-  text: string;
-}
-
-const InnerForm: React.SFC<
-  InjectedFormikProps<FormProps, FormValues>
-> = props => {
-  const clickAdd = (_: React.MouseEvent) => {
-    props.add(props.values.text);
-    props.submitForm();
-  };
-  const clickAsyncAdd = (_: React.MouseEvent) => {
-    props.asyncAdd(props.values.text);
-    props.submitForm();
-  };
-  return (
-    <form>
-      <TextField
-        label="add todo"
-        name="text"
-        value={props.values.text}
-        placeholder="input text"
-        // InputLabelProps={{
-        //   shrink: true,
-        // }}
-        onChange={props.handleChange}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        onClick={clickAdd}
-        disabled={props.values.text.length <= 0 || props.isSubmitting}
-      >
-        add
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        onClick={clickAsyncAdd}
-        disabled={props.values.text.length <= 0 || props.isSubmitting}
-      >
-        async add
-      </Button>
-      <Button variant="contained" color="secondary" onClick={props.handleReset}>
-        reset
-      </Button>
-    </form>
-  );
-};
-
-const EnhancedForm = withFormik<FormProps, FormValues>({
-  mapPropsToValues: () => ({ text: '' }),
-  handleSubmit: (values, { setSubmitting, resetForm }) => {
-    if (values.text.trim().length <= 0) return;
-    resetForm();
-    setSubmitting(false);
-  },
-})(InnerForm);
-
-const TodoForm = pure(EnhancedForm);
 
 export const TodoComponent: React.SFC<Props> = ({
   todos,
