8b009658d7daf75a18947466f3628ed8ea9f96bd
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-20 10:40:30 +0900

    fix: remove unnecessary module for CSR

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,31 +1,19 @@
 import loadable from 'loadable-components';
 
-export const LoadableHome = loadable(
-  () =>
-    import(/* webpackChunkName: "Home" */ '../components/pages/Home').then(
-      ({ Home }) => Home,
-    ),
-  {
-    modules: ['../components/pages/Home'],
-  },
+export const LoadableHome = loadable(() =>
+  import(/* webpackChunkName: "Home" */ '../components/pages/Home').then(
+    ({ Home }) => Home,
+  ),
 );
 
-export const LoadableCounter = loadable(
-  () =>
-    import(/* webpackChunkName: "Counter" */ '../containers/Counter').then(
-      ({ Counter }) => Counter,
-    ),
-  {
-    modules: ['../containers/Counter'],
-  },
+export const LoadableCounter = loadable(() =>
+  import(/* webpackChunkName: "Counter" */ '../containers/Counter').then(
+    ({ Counter }) => Counter,
+  ),
 );
 
-export const LoadableTodo = loadable(
-  () =>
-    import(/* webpackChunkName: "Todo" */ '../containers/Todo').then(
-      ({ Todo }) => Todo,
-    ),
-  {
-    modules: ['../../containers/Todo'],
-  },
+export const LoadableTodo = loadable(() =>
+  import(/* webpackChunkName: "Todo" */ '../containers/Todo').then(
+    ({ Todo }) => Todo,
+  ),
 );
