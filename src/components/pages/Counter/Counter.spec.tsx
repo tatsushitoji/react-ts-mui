e2fa7d75bef8f4cb103095dbad50a54e6c802f00
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-11-28 16:43:47 +0900

    test: fix failed

--- src/components/pages/Counter/Counter.spec.tsx
+++ src/components/pages/Counter/Counter.spec.tsx
@@ -1,25 +1,20 @@
 import * as React from 'react';
-import { createShallow } from '@material-ui/core/test-utils';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import { Counter } from '.';
-
-const sel = (id: string) => {
-  return `[data-test="${id}"]`;
-};
 
 let mockIncrement: any;
 let mockDecrement: any;
 let mockAsyncIncrement: any;
-let shallow: any;
 
 beforeEach(() => {
-  shallow = createShallow();
   mockIncrement = jest.fn();
   mockDecrement = jest.fn();
   mockAsyncIncrement = jest.fn();
 });
 
 test('should render self with initial state', () => {
-  const wrapper = shallow(
+  const tree = shallow(
     <Counter
       count={0}
       increment={mockIncrement}
@@ -27,44 +22,5 @@ test('should render self with initial state', () => {
       asyncIncrement={mockAsyncIncrement}
     />,
   );
-  expect(wrapper.find(sel('counter')).text()).toContain('0');
-});
-
-test('should call increment button click', () => {
-  const wrapper = shallow(
-    <Counter
-      count={0}
-      increment={mockIncrement}
-      decrement={mockDecrement}
-      asyncIncrement={mockAsyncIncrement}
-    />,
-  );
-  wrapper.find(sel('increment-button')).simulate('click');
-  expect(mockIncrement.mock.calls.length).toEqual(1);
-});
-
-test('should call decrement button click', () => {
-  const wrapper = shallow(
-    <Counter
-      count={0}
-      increment={mockIncrement}
-      decrement={mockDecrement}
-      asyncIncrement={mockAsyncIncrement}
-    />,
-  );
-  wrapper.find(sel('decrement-button')).simulate('click');
-  expect(mockDecrement.mock.calls.length).toEqual(1);
-});
-
-test('should call asyncIecrement button click', () => {
-  const wrapper = shallow(
-    <Counter
-      count={0}
-      increment={mockIncrement}
-      decrement={mockDecrement}
-      asyncIncrement={mockAsyncIncrement}
-    />,
-  );
-  wrapper.find(sel('async-increment-button')).simulate('click');
-  expect(mockAsyncIncrement.mock.calls.length).toEqual(1);
+  expect(toJson(tree)).toMatchSnapshot();
 });
